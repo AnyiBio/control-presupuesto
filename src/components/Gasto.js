8dd472defb058219169709cb95a6664ae781388d
Anyi Jaramillo <anyi.jar@gmail.com>
2020-04-03 05:12:57 +0200

    first commit

---
+++ src/components/Gasto.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Gasto = ({ gasto }) => (
+  <li className="gastos">
+    <p>
+      {gasto.nombre}
+      <span className="gasto">€ {gasto.cantidad}</span>
+    </p>
+  </li>
+);
+Gasto.protoTypes = {
+  gastos: PropTypes.object.isRequired
+};
+
+export default Gasto;
